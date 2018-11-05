251f9b7a2d3b4e2f17723142bd9a9b01a9c5adeb
Sebastian Choo <sebastian_choo_yong_qiang@s2017.sst.edu.sg>
2018-11-05 12:27:38 +0800

    Initial Commit

---
+++ Quizz/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Quizz
+//
+//  Created by Sebastian Choo on 5/11/18.
+//  Copyright © 2018 SebastianChoo.Co. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
